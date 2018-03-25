e8165bf6992ed55f5e4966da774550203715525e
Gao Shuqi <gaoshq@outlook.com>
2018-03-25 12:58:19 +0800

    Qualification Round 2015, Problem D, Unfinished.

---
+++ Google_Code_Jam/Ominous_Omino.cpp
@@ -0,0 +1,22 @@
+/*
+Google Code Jam, Qualification Round 2015, Problem D. Ominous Omino
+Retrieved from https://code.google.com/codejam/contest/6224486/dashboard#s=p3
+By Gao Shuqi, on 2018-03-23.
+*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+	freopen("C-large-practice.in", "r", stdin);
+	freopen("C-large-practice.out", "w", stdout);
+	int T;
+	scanf("%d\n", &T);
+	
+
+	fclose(stdin);
+	fclose(stdout);
+	return 0;
+}
