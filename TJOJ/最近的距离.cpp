ff6a26dc1ae12d84f1f121f3c0e67c69a809f42c
Gao Shuqi <gaoshq@outlook.com>
2018-03-25 20:06:40 +0800

    Tongji OJ, 网络赛 2017, Unfinished.

---
+++ TJOJ/最近的距离.cpp
@@ -0,0 +1,17 @@
+/*
+Tongji Open Judge, 网络赛 2017, Problem B. 最近的距离
+Retrieved from http://acm.tongji.edu.cn/contest.php?cid=1002
+Finished, By Gao Shuqi, on 2018-03-25.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double x1, y1, dx1, dy1, x2, y2, dx2, dy2;
+	double ans = 0.0;
+
+
+	printf("%.9f", ans);
+	return 0;
+}
