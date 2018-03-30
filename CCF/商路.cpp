25950e4b99548a306b1ca5f29050c87b658b2abb
Gao Shuqi <gaoshq@outlook.com>
2018-03-30 09:54:47 +0800

    CCF 201712-5, Finished.

--- CCF/商路.cpp
+++ CCF/商路.cpp
@@ -18,6 +18,11 @@ struct Tree{
 	int parent;
 	int dist;
 }tree[100001];
+
+struct Node{
+	int name;
+	int dist;
+};
 queue<int> child[100001];
 long long value[100001];
 bool visited[100001];
@@ -27,17 +32,38 @@ long long cal(int d) {
 	if (visited[d]) return value[d];
 	if (child[d].empty()) { value[d] = 0; visited[d] = true; return 0; }
 	int length = child[d].size();
-	long long maxium = 0xffffffff;
-	for (int i = 1; i <= length; i++) {
-		int p = child[d].front();
+	queue<Node> posterior;
+	while (!posterior.empty()) posterior.pop();
+	for (int i = 0; i < length; i++) {
+		int tmp_d = child[d].front();
 		child[d].pop();
-		int dist_base = 0;
-
-		long long tmp = tree[d].data.v - (tree[d].data.f - tree[p].dist)*((tree[d].data.f - tree[p].dist));
-		tmp %= 1000000000000000000;
-		tmp += cal(p);
-		tmp %= 1000000000000000000;
-		if (tmp > maxium) maxium = tmp;
+		child[d].push(tmp_d);
+		Node tmp_N;;
+		tmp_N.name = tmp_d;
+		tmp_N.dist = tree[tmp_d].dist;
+		posterior.push(tmp_N);
+	}
+	long long maxium = 0;
+	while (!posterior.empty()) {
+		Node tmp = posterior.front();
+		posterior.pop();
+		if (!child[tmp.name].empty()) {
+			int length = child[tmp.name].size();
+			for (int i = 0; i < length; i++) {
+				int tmp_d = child[tmp.name].front();
+				child[tmp.name].pop();
+				child[tmp.name].push(tmp_d);
+				Node tmp_N;;
+				tmp_N.name = tmp_d;
+				tmp_N.dist = tree[tmp_d].dist+tmp.dist;
+				posterior.push(tmp_N);
+			}
+		}
+		long long tmp_ans = tree[d].data.v - (tree[d].data.f - tmp.dist)*((tree[d].data.f - tmp.dist));
+		tmp_ans %= 1000000000000000000;
+		tmp_ans += cal(tmp.name);
+		tmp_ans %= 1000000000000000000;
+		if (tmp_ans > maxium) maxium = tmp_ans;
 	}
 	value[d] = maxium;
 	visited[d] = true;
