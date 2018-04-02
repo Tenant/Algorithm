bfa8a7cfef819d989f5945f468ac352052fd13df
Gao Shuqi <gaoshq@outlook.com>
2018-04-02 11:16:30 +0800

    回溯问题, 用45度/135度斜线代替主对角线概念，Finished.

--- Programming Challenges/八皇后问题.cpp
+++ Programming Challenges/八皇后问题.cpp
@@ -10,21 +10,21 @@ Finished, By Gao Shuqi, on 2018-04-02
 using namespace std;
 int N; /*N<=M_MAX*/
 long long cnt;
-bool col[N_MAX+1], diag[3];
+bool col[N_MAX+1], diag45[2*N_MAX], diag135[2*N_MAX+1];
 
 void visit(int d) {
 		for (int j = 1; j <= N; j++) {
 			if (col[j]) continue;
-			if (d == j && diag[1]) continue;
-			if (d + j == N + 1 && diag[2]) continue;
+			if (diag45[j-d+N]) continue;
+			if (diag135[d+j-1]) continue;
 			col[j] = true;
-			if (d == j) diag[1] = true;
-			if (d + j == N + 1) diag[2] = true;
+			diag45[j - d + N] = true;
+			diag135[d + j - 1] = true;
 			if (d == N) cnt++;
 			else visit(d+1);
 			col[j] = false;
-			if (d == j) diag[1] = false;
-			if (d + j == N + 1) diag[2] = false;
+			diag45[j - d + N] = false;
+			diag135[d + j - 1] = false;
 	}
 }
 
@@ -33,7 +33,8 @@ int main() {
 	scanf("%d", &T);
 	for (int ii = 1; ii <= T; ii++) {
 		memset(col, 0, sizeof(col));
-		memset(diag, 0, sizeof(diag));
+		memset(diag45, 0, sizeof(diag45));
+		memset(diag135, 0, sizeof(diag135));
 		scanf("%d", &N);
 		cnt = 0;
 		visit(1);
