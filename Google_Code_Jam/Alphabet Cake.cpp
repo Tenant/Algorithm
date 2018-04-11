a0115afde1b315d5ea7e5987d21597fede4f9140
Gao Shuqi <gaoshq@outlook.com>
2018-04-11 09:52:00 +0800

    Round 1A 2017, Problem A, 分治法, Almost.

--- Google_Code_Jam/Alphabet Cake.cpp
+++ Google_Code_Jam/Alphabet Cake.cpp
@@ -1,83 +1,67 @@
 ﻿/*
 Google Code Jam, Round 1A 2017, Problem A. Alphabet Cake
-Retrieved from https://code.google.com/codejam/contest/5304486/dashboard
-Completed, By Gao Shuqi, on 2018-04-06
+Retrieved from https://code.google.com/codejam/contest/5304486/dashboard#s=p1
+分治法
+By Gao Shuqi, on 2018-04-11.
 */
 
 #include <iostream>
-#include <stdio.h>
 #define N_MAX 25
 using namespace std;
 
-char map[N_MAX + 1][N_MAX + 1];
-bool visited[N_MAX + 1][N_MAX + 1];
 int R, C;
+char map[N_MAX + 1][N_MAX + 1];
+int cnt[N_MAX + 1];
+int mark[N_MAX + 1],poc;
 
-void deal(int x, int y) {
-	int left = y-1;
-	for (; left >= 1 && map[x][left] == '?' && !visited[x][left]; left--);
-	left++;
-	int top = x-1;
-	for (; top >= 1 && map[top][0] == '0'; top--);
-	top++;
-	int right = y+1;
-	bool tm = true;
-	while (tm && right<=C) {
-		bool tm1 = true;
-		for (int i = top; right<C && i <= x; i++) {
-			if (visited[i][right] || map[i][right] != '?') { tm1 = false; break;}
-		}
-		if (tm1) right++;
-		else tm = false;
-	}
-	if(!tm) right--;
-	tm = true;
-	for (int i = x + 1; i <= R; i++) {
-		if (map[i][0] != '0') { tm = false; break; }
-	}
-	int bottom = tm ? C : x;
+void fill(int left, int right, int top, int bottom, char c) {
 	for (int i = top; i <= bottom; i++) {
 		for (int j = left; j <= right; j++) {
-			map[i][j] = map[x][y];
-			visited[i][j] = true;
+			map[i][j] = c;
 		}
 	}
-	
 }
 
 int main() {
-	//freopen("A-small-practice.in", "r", stdin);
-	//freopen("A-small-practice.out", "w", stdout);
 	int T;
 	scanf("%d", &T);
 	for (int ii = 1; ii <= T; ii++) {
 		scanf("%d%d", &R, &C);
-		getchar();
 		for (int i = 1; i <= R; i++) {
-			char str[N_MAX + 1];
-			gets(str);
 			int tmp = 0;
 			for (int j = 1; j <= C; j++) {
-				map[i][j] = str[j - 1];
-				if (str[j - 1] != '?') tmp++;
+				cin >> map[i][j];
+				if (map[i][j] != '?') tmp++;
 			}
-			map[i][0] = '0' + tmp;
+			cnt[i] = tmp;
 		}
-		memset(visited, 0, sizeof(visited));
-		for (int i = 1; i <= R; i++) {
-			for (int j = 1; j <= C; j++) {
-				if (map[i][j] != '?') deal(i, j);
+		int last_row = R;
+		for (; last_row > 0 && cnt[last_row] == 0; last_row--);
+		int current_row = 1;
+		while (current_row <= R) {
+			int tmp = current_row;
+			for (; current_row <= R && cnt[current_row] == 0; current_row++);
+			poc = 0;
+			for (int i = 1; i <= C; i++) {
+				if (map[current_row][i] != '?') mark[++poc] = i;
 			}
+			int bottom = (current_row == last_row) ? R : current_row;
+			if (cnt[current_row] == 1) fill(1, C, tmp, bottom, map[current_row][mark[1]]);
+			else fill(1, mark[2]-1, tmp, bottom, map[current_row][mark[1]]);
+			for (int i = 1; i < cnt[current_row]; i++) {
+				int right = (i == cnt[current_row] - 1) ? C : mark[i + 1] - 1;
+				fill(mark[i] + 1, right, tmp, bottom, map[current_row][mark[i]]);
+			}
+			if (bottom = R) break;
+			current_row++;
 		}
-
 		printf("Case #%d:\n", ii);
-		for (int i = 1; i <= R; i++) {
-			for (int j = 1; j <= C; j++)
-				printf("%c", map[i][j]);
+		for (int i = 1; i <= R; i++){
+			printf("%c", map[i][1]);
+			for (int j = 2; j <= C; j++)
+				printf(" %c", map[i][j]);
 			printf("\n");
 		}
 	}
-	//fclose(stdin);
-	//fclose(stdout);
 	return 0;
 }
