5b6affca91be4941e4ad6c1caaf8bdfe759bb3a7
Gao Shuqi <gaoshq@outlook.com>
2018-04-15 12:28:59 +0800

    Problem 1086, Unfinished.

--- TJOJ/硬币翻转.cpp
+++ TJOJ/硬币翻转.cpp
@@ -1,50 +1,25 @@
 ﻿/*
-Tongji Open Judge, 网络赛 2017, Problem C. 硬币翻转
-Retrieved from http://acm.tongji.edu.cn/problem.php?cid=1001&pid=2
-Finished, By Gao Shuqi, on 2018-03-26.
+Tongji OJ, Problem 1085. 硬币翻转
+Retrieved from http://acm.tongji.edu.cn/problem.php?id=1085
+归纳法
+Completed, By Gao Shuqi, on 2018-03-26.
 */
 
 #include <iostream>
-#include <queue>
 using namespace std;
-long long Min(long a, long b) { return (a < b) ? a : b; }
-
-struct E{
-	long long num;
-	int dist;
-};
 
 int main() {
 	long long N, M, K;
 	scanf("%lld%lld%lld", &N, &M, &K);
-	int cnt;
-	if (K == 1) cnt = Min(N, M);
-	else if (M%K == 0 && N%K != 0) cnt = M / K;
-	else if (M%K != 0 && N%K == 0) cnt = N / K;
-	else if (M%K == 0 && N%K == 0) cnt = Min(M, N) / K;
-	else {//Breadth First Search
-		long long Total = N + M;
-		queue<E> q;
-		while (!q.empty()) q.pop();
-		E tmp;
-		tmp.num = N;
-		tmp.dist = 0;
-		q.push(tmp);
-		while (q.front().num != 0 && q.front().num != Total) {
-			long long tmp_n = q.front().num;
-			int tmp_d = q.front().dist;
-			q.pop();
-			for (int i = 0; i <= K; i++) {
-				tmp.num = tmp_n + i - (K - i);
-				tmp.dist = tmp_d + 1;
-				q.push(tmp);
-			}
-		}
-		cnt = q.front().dist;
-	}
+	long long L = N;
+	long long S = M;
+	long long cnt;
+	long long n = S / K;
+	if (K == 1) cnt = S;
+	else if (K == S) cnt == 1;
+	else if (K < S && ((S-(n-1)*K) % 2 == 0)) cnt = S / K + 1;
+	else cnt = -1;
 	
-	printf("%d\n", cnt);
-	//getchar();
-	//getchar();
+	printf("%lld\n", cnt);
 	return 0;
 }
