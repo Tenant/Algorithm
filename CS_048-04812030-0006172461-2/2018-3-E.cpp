89cd283b8e71d48fd32e40edf390c46fe2950335
Gao Shuqi <30630975+Tenant@users.noreply.github.com>
2018-11-23 11:21:05 +0800

    2018-3-E accepted
    动态规划股票交易问题

--- CS_048-04812030-0006172461-2/2018-3-E.cpp
+++ CS_048-04812030-0006172461-2/2018-3-E.cpp
@@ -1,33 +1,41 @@
 //
-// Created by Dell on 11/18/2018.
+// Source: http://algorithm.openjudge.cn/2018test3/E/
+// Type: Dynamic Programming
+// Solved by Sukie on 11/18/2018.
 //
 #include <iostream>
 #define N_MAX 100000
 using namespace std;
 
-int stock_purchage(int *data, int length){
-    int profit = 0;
-    int dp_low[N_MAX], dp_high[N_MAX];
-    dp_low[0]=data[0];
-    dp_high[0]=data[0];
-    for(int idx=1;idx<N;idx++){
-
-    }
-
-
-    return profit;
-}
+int max(int a, int b){ return (a > b) ? a : b; }
 
 int main(){
-    int T;
-    scanf("%d",&T);
-    while(T--){
-        int N;
-        scanf("%d",&N);
-        int prices[N_MAX];
-        for(int idx=0;idx<N;idx++) scanf("%d",&prices[idx]);
-        int answer=stock_purchage(prices,N);
-        printf("%d\n",answer);
-    }
-    return 0;
+	int T;
+	scanf("%d", &T);
+	while (T--){
+		int N;
+		scanf("%d", &N);
+		int prices[N_MAX];
+		for (int idx = 0; idx<N; idx++) scanf("%d", &prices[idx]);
+		
+		int dp[N_MAX][3][2];
+		dp[0][0][0] = dp[0][1][0] = dp[0][2][0] = 0;
+		dp[0][0][1] = dp[0][1][1] = dp[0][2][1] = -1;
+		for (int idx = 1; idx < N; idx++) {
+			dp[idx][0][0] = 0;
+			dp[idx][0][1] = -1;
+		}
+		for (int idx = 1;idx<=N; idx++){
+			for (int p = 1; p <= 2; p++){
+				if (dp[idx - 1][p][1] == -1) dp[idx][p][0] = dp[idx - 1][p][0];
+				else dp[idx][p][0] = max(dp[idx - 1][p][0], dp[idx - 1][p][1] + prices[idx - 1]);
+				if (dp[idx - 1][p][1] == -1) dp[idx][p][1] = dp[idx - 1][p - 1][0] - prices[idx - 1];
+				else dp[idx][p][1] = max(dp[idx - 1][p][1], dp[idx - 1][p - 1][0] - prices[idx - 1]);
+			}
+		}
+
+		int answer = dp[N][2][0];
+		printf("%d\n", answer);
+	}
+	return 0;
 }
