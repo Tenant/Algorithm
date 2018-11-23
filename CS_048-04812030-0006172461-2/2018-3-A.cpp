26c82e94f8c900cff1675bdf619075a627dd03bf
Gao Shuqi <30630975+Tenant@users.noreply.github.com>
2018-11-23 13:20:21 +0800

    2018-3-A TLE

--- CS_048-04812030-0006172461-2/2018-3-A.cpp
+++ CS_048-04812030-0006172461-2/2018-3-A.cpp
@@ -4,64 +4,64 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#define N_MAX 100000
+#define N_MAX 1000
 
 using namespace std;
 
 class Point2d{
 public:
-    int x;
-    int y;
+	int x;
+	int y;
 };
 
 bool compare_X(Point2d a, Point2d b){
-    return a.x <= b.x;
+	return a.x < b.x;
 }
 
 bool compare_Y(Point2d a, Point2d b){
-    return a.y <= b.y;
+	return a.y < b.y;
 }
 
 Point2d points[N_MAX];
 
 
 double closest_pair(Point2d* data, int length){
-    for (int idx = 0; idx < length; idx++) printf("(%d,%d)\t", data[idx].x, data[idx].y);
-    printf("\n");
-    if (length == 1) return -1;
-    if (length == 2) return sqrt((data[0].x - data[1].x)*(data[0].x - data[1].x) + (data[0].y - data[1].y)*(data[0].y - data[1].y));
-    double sigma1 = closest_pair(data, length / 2);
-    double sigma2 = closest_pair(data + length / 2, length - length / 2);
-    double sigma = (sigma1<sigma2) ? sigma1 : sigma2;
-    double L = (data[length / 2 - 1].x + data[length / 2].x) / 2.;
-    Point2d local_points[N_MAX];
-    for (int idx = 0; idx< length; idx++){
-        if (abs(data[idx].x - L) > sigma) continue;
-        int head = (idx - 5)>0 ? (idx - 5) : 0;
-        int tail = (idx + 5)<length ? (idx + 5) : (length - 1);
-        sort(data + head, data + tail + 1, compare_Y);
-        for (int p = head; p <= tail; p++){
-            double temp_dist = sqrt((data[p].x - data[idx].x)*(data[p].x - data[idx].x) + (data[p].y - data[idx].y) * (data[p].y - data[idx].y));
-            if (temp_dist < sigma) sigma = temp_dist;
-        }
+	if (length == 1) return -1;
+	if (length == 2) return sqrt((data[0].x - data[1].x)*(data[0].x - data[1].x) + (data[0].y - data[1].y)*(data[0].y - data[1].y));
+	double sigma1 = closest_pair(data, length / 2);
+	double sigma2 = closest_pair(data + length / 2, length - length / 2);
+	double sigma;
+	if (sigma1 == -1) sigma = sigma2;
+	else if (sigma2 == -1) sigma = sigma1;
+	else sigma = (sigma1>sigma2) ? sigma2 : sigma1;
+	double L = (data[length / 2 - 1].x + data[length / 2].x) / 2.;
+	for (int idx = 0; idx< length; idx++){
+		if (abs(data[idx].x - L) > sigma) continue;
+		int head = (idx - 5)>0 ? (idx - 5) : 0;
+		int tail = (idx + 5)<length ? (idx + 5) : (length - 1);
+		sort(data + head, data + tail + 1, compare_Y);
+		for (int p = head; p <= tail; p++){
+			if (p == idx) continue;
+			double temp_dist = sqrt((data[p].x - data[idx].x)*(data[p].x - data[idx].x) + (data[p].y - data[idx].y) * (data[p].y - data[idx].y));
+			if (temp_dist < sigma) sigma = temp_dist;
+		}
 
-    }
-    return sigma;
+	}
+	return sigma;
 
 }
 
 
 int main(){
-    int T;
-    scanf("%d", &T);
-    while (T--){
-        int N;
-        scanf("%d", &N);
-        for (int idx = 0; idx<N; idx++) scanf("%d%d", &points[idx].x, &points[idx].y);
-        sort(points, points + N, compare_X);
-        double answer = closest_pair(points, N);
-        printf("%.6f\n", answer);
-    }
-    return 0;
-};
-
+	int T;
+	scanf("%d", &T);
+	while (T--){
+		int N;
+		scanf("%d", &N);
+		for (int idx = 0; idx<N; idx++) scanf("%d%d", &points[idx].x, &points[idx].y);
+		sort(points, points + N, compare_X);
+		double answer = closest_pair(points, N);
+		printf("%.6f\n", answer);
+	}
+	return 0;
+}
