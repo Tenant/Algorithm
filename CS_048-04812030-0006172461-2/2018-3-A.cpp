1dec0727a2e488a9836768b1c031d32b6b5227b7
Gao Shuqi <30630975+Tenant@users.noreply.github.com>
2018-11-28 09:53:01 +0800

    modify inner sorting with MergeSort, ETL

--- CS_048-04812030-0006172461-2/2018-3-A.cpp
+++ CS_048-04812030-0006172461-2/2018-3-A.cpp
@@ -27,7 +27,15 @@ Point2d points[N_MAX];
 
 double closest_pair(Point2d* data, int length){
 	if (length == 1) return -1;
-	if (length == 2) return sqrt((data[0].x - data[1].x)*(data[0].x - data[1].x) + (data[0].y - data[1].y)*(data[0].y - data[1].y));
+	if (length == 2) {
+		if (data[0].y>data[1].y){
+			Point2d temp_point;
+			temp_point = data[0];
+			data[0] = data[1];
+			data[1] = temp_point;
+		}
+		return sqrt((data[0].x - data[1].x)*(data[0].x - data[1].x) + (data[0].y - data[1].y)*(data[0].y - data[1].y));
+	}
 	double sigma1 = closest_pair(data, length / 2);
 	double sigma2 = closest_pair(data + length / 2, length - length / 2);
 	double sigma;
@@ -36,24 +44,24 @@ double closest_pair(Point2d* data, int length){
 	else sigma = (sigma1>sigma2) ? sigma2 : sigma1;
 	double L = (data[length / 2 - 1].x + data[length / 2].x) / 2.;
 	Point2d localpoints[N_MAX];
-	int locallength=0;
-	for (int idx = 0, locallength = 0; idx<length;idx++)
-	if (abs(points[idx].x - L) <= L)
-		localpoints[locallength++] = points[idx];
-	sort(localpoints, localpoints + locallength, compare_Y);
-	int x;
-	for (int idx = 0; idx<locallength; idx++){
-		if (localpoints[idx].x <= L) continue;
-		x = 0;
-		for (int p = idx + 1; p <= idx + 6 + x && p<locallength; p++){
-			if (localpoints[p].x < L){
-				x++;
-				continue;
-			}
-			double temp_dist = sqrt((data[p].x - data[idx].x)*(data[p].x - data[idx].x) + (data[p].y - data[idx].y) * (data[p].y - data[idx].y));
-			if (temp_dist<sigma){
-				sigma = temp_dist;
-			}
+	int local_cnt = 0;
+	int i=0, j=length/2;
+	for (; i < length / 2 && j < length;){
+		if (data[i].y <= data[j].y) localpoints[local_cnt++] = data[i++];
+		else localpoints[local_cnt++] = data[j++];
+	}
+	if (i<length/2)
+	for (; i < length / 2;)
+		localpoints[local_cnt++] = data[i++];
+	else
+	for (; j < length;)
+		localpoints[local_cnt++] = data[j++];
+	data = localpoints;
+	for (int idx = 0; idx<length; idx++){
+		if (abs(localpoints[idx].x - L)>=sigma) continue;
+		for (int p = idx + 1; p < idx + 7 && p < length; p++){
+			double temp_dist = sqrt((data[idx].x - data[p].x)*(data[idx].x - data[p].x) + (data[idx].y - data[p].y)*(data[idx].y - data[p].y));
+			if (temp_dist < sigma) sigma = temp_dist;
 		}
 	}
 	return sigma;
