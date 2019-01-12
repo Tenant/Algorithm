0bb714358596556726cdbc794acdb438c8ae10bd
sukie <gaoshuqi@pku.edu.cn>
2019-01-13 06:15:08 +0800

    add Template of Knapsack Problem

--- Template/Knapsack.cpp
+++ Template/Knapsack.cpp
@@ -2,16 +2,17 @@
 #include <limits.h>
 using namespace std;
 
-const int W_MAX = 10000 + 1;
-const int N_MAX = 500;
+const int W_MAX = 12880 + 1;
+const int N_MAX = 3402;
 const int MAX_INT = 1e10;
 
 int W, N;
 int w[N_MAX], v[N_MAX];
 
+int max(int a, int b) { return (a > b) ? a : b; }
 int min(int a, int b) { return (a < b) ? a : b; }
 
-int dp(){
+int knapsack_min(){
 	int dp[W_MAX]; dp[0] = 0; for (int i = 1; i <= W; i++) dp[i] = MAX_INT;
 	for (int p = 0; p < N; p++)
 	for (int i = w[p]; i <= W; i++)
@@ -20,18 +21,36 @@ int dp(){
 	else return dp[W];
 }
 
+int knapsack_01_min(){
+	int dp[W_MAX]; dp[0] = 0; for (int i = 1; i <= W; i++) dp[i] = MAX_INT;
+	for (int p = 0; p < N; p++)
+	for (int i = W; i >= w[p]; i--)
+		dp[i] = min(dp[i], dp[i - w[p]] + v[p]);
+	if (dp[W] == MAX_INT) return -1;
+	else return dp[W];
+}
+
+int knapsack_max(){
+	int dp[W_MAX]; dp[0] = 0; for (int i = 1; i <= W; i++) dp[i] = 0;
+	for (int p = 0; p < N; p++)
+	for (int i = w[p]; i <= W; i++)
+		dp[i] = max(dp[i], dp[i - w[p]] + v[p]);
+	return dp[W];
+}
+
+int knapsack_01_max(){
+	int dp[W_MAX]; dp[0] = 0; for (int i = 1; i <= W; i++) dp[i] = 0;
+	for (int p = 0; p < N; p++)
+	for (int i = W; i >= w[p]; i--)
+		dp[i] = max(dp[i], dp[i - w[p]] + v[p]);
+	return dp[W];
+}
+
 int main(){
-	int T;
-	scanf("%d", &T);
-	while (T--){
-		int E, F;
-		scanf("%d%d", &E, &F);
-		W = F - E;
-		scanf("%d", &N);
-		for (int i = 0; i < N; i++) scanf("%d%d", &v[i], &w[i]);
-		int answer = dp();
-		if (answer == -1) printf("This is impossible.\n");
-		else printf("The minimum amount of money in the piggy-bank is %d.\n", answer);
+	while (scanf("%d%d", &N, &W) != EOF /*&& !(N==0 && W==0)*/){
+		for (int i = 0; i < N; i++) scanf("%d%d", &w[i], &v[i]);
+		int answer = knapsack_01_max();
+		printf("%d\n", answer);
 	}
 	return 0;
 }
